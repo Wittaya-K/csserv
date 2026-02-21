398d54ffe740fc9dd194a2b4f2acfac7c4f5d35d
wittaya khuanwilai <wittaya.khuanwilai@hotmail.com>
2026-02-21 15:59:21 +0700

    css , script commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
     <title>{{ trans('global.system') }}</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="/css/adminltev3.css" rel="stylesheet" />
+    <link href="{{ asset('css/adminltev3.css') }}" rel="stylesheet" />
     <link href="{{ asset('font-awesome-pro-5.15.4/css/all.min.css') }}" rel="stylesheet" />
     <style> 
         @font-face {
